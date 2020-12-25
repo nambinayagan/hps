d3e21e176d8de9e74a64c99ed9a7e5aa529d098c
Nambinayagan <75838920+nambinayagan@users.noreply.github.com>
2020-12-25 22:31:11 +0530

    added user verification

--- diskusage.sh
+++ diskusage.sh
@@ -1,13 +1,16 @@
-#!/bin/bash
-echo Enter username
-read user1
+bash << 'EOF'
+user1="atestvwk"
+#echo Enter username
+#read user1
 docroot1=$( /root/bin/ui $user1 | grep "Doc Root" | cut -d":" -f2 )
+[ -z "$docroot1" ] && exit;
 docroot2=$( echo $docroot1 | sed 's/public_html//g' )
 title1="###################################################"
-printf "%*s\n" $(((${#title1}+$COLUMNS)/2)) "$title1"
 COLUMNS=$(tput cols)
+printf "%*s\n" $(((${#title1}+$COLUMNS)/2)) "$title1"
 title="Complete Disk usage"
 printf "%*s\n" $(((${#title}+$COLUMNS)/2)) "$title"
+#echo -e "#############################################################################"
 title1="###################################################"
 printf "%*s\n" $(((${#title1}+$COLUMNS)/2)) "$title1"
 #echo "Complete disk usage"
@@ -17,5 +20,9 @@ printf "%*s\n" $(((${#title1}+$COLUMNS)/2)) "$title1"
 title2="Disk usage by Mail"
 printf "%*s\n" $(((${#title2}+$COLUMNS)/2)) "$title2"
 printf "%*s\n" $(((${#title1}+$COLUMNS)/2)) "$title1"
-du -hac $docroot2/mail  | sort -rh | head -10
+
+du -hac $docroot2/mail  | sort -rh | head -25
 printf "%*s\n" $(((${#title1}+$COLUMNS)/2)) "$title1"
+
+#echo -e "#############################################################################"
+EOF
